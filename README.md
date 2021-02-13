7489a529f2b2981b2573b570425981a86eb6024c
Ryan W <rwesslen@gmail.com>
2021-02-13 15:38:21 -0500

    update for stackoverflow data and readme instructions

--- README.md
+++ README.md
@@ -1,11 +1,15 @@
 # Building a semantic search engine with Transformers and Faiss
-This repository contains the code for the following Medium blogs:
+This post was created with reference from:
 - [How to build a semantic search engine with Transformers and Faiss](https://kstathou.medium.com/how-to-build-a-semantic-search-engine-with-transformers-and-faiss-dcbea307a0e8?source=friends_link&sk=6974c79b86e2f257c32f77d49583a524)
 - [How to deploy a machine learning model on AWS Elastic Beanstalk with Streamlit and Docker](https://kstathou.medium.com/how-to-deploy-a-semantic-search-engine-with-streamlit-and-docker-on-aws-elastic-beanstalk-42ddce0422f3?source=friends_link&sk=dcc7bbf8d172f2cd18aefcdf0c2c6b49)
 
 Check out the blogs if you want to learn how to create a semantic search engine with Sentence Transformers and Faiss.  
 
 You can [run the notebook on Google Colab](https://colab.research.google.com/github/kstathou/vector_engine/blob/master/notebooks/001_vector_search.ipynb) and leverage the free GPU to speed up the computation!
+
+## Data
+
+Data has been replaced with sample of StackOverflow: see https://gist.github.com/wesslen/1bb5a860723e74fdb85295c6c913783c
 
 ## How to deploy the Streamlit app locally with Docker ##
 Assuming docker is running on your machine and you have a docker account, do the following:
@@ -22,3 +26,62 @@ docker run -p 8501:8501 <USERNAME>/<YOUR_IMAGE_NAME>
 ```
 
 - Open your browser and go to `http://localhost:8501/`
+
+## How to modify data set used / create index
+
+``` python
+import pandas as pd
+
+# Used to create the dense document vectors.
+import torch
+from sentence_transformers import SentenceTransformer
+
+# Used to create and store the Faiss index.
+import faiss
+import numpy as np
+import pickle
+from pathlib import Path
+
+# Used to do vector searches and display the results.
+from vector_engine.utils import vector_search, id2details
+
+# assume file is in data/posts.json
+pd.read_json("data/posts.json")
+
+# Instantiate the sentence-level DistilBERT
+model = SentenceTransformer('distilbert-base-nli-stsb-mean-tokens')
+
+# Check if GPU is available and use it
+if torch.cuda.is_available():
+    model = model.to(torch.device("cuda"))
+print(model.device)
+
+# Convert abstracts to vectors
+embeddings = model.encode(posts.title.to_list(), show_progress_bar=True)
+
+# Step 1: Change data type
+embeddings = np.array([embedding for embedding in embeddings]).astype("float32")
+
+# Step 2: Instantiate the index
+index = faiss.IndexFlatL2(embeddings.shape[1])
+
+# Step 3: Pass the index to IndexIDMap
+index = faiss.IndexIDMap(index)
+
+# Step 4: Add vectors and their IDs
+index.add_with_ids(embeddings, posts.id.values)
+
+print(f"Number of vectors in the Faiss index: {index.ntotal}")
+
+# Define project base directory
+# Change the index from 1 to 0 if you run this on Google Colab
+project_dir = Path('notebooks').resolve().parents[1]
+print(project_dir)
+
+# Serialise index and store it as a pickle
+with open(f"{project_dir}/models/stack_faiss_index.pickle", "wb") as h:
+    pickle.dump(faiss.serialize_index(index), h)
+
+```
+
+Then update the `app.py` file to modify the data being loaded (update according columns) and the new index (pickle file).
--- app.py
+++ app.py
@@ -7,9 +7,9 @@ from vector_engine.utils import vector_search
 
 
 @st.cache
-def read_data(s3_data="s3://vector-search-blog/misinformation_papers.csv"):
+def read_data(get_posts="data/posts.json"):
     """Read the data from S3."""
-    return pd.read_csv(s3_data)
+    return pd.read_json(get_posts)
 
 
 @st.cache(allow_output_mutation=True)
@@ -19,7 +19,7 @@ def load_bert_model(name="distilbert-base-nli-stsb-mean-tokens"):
 
 
 @st.cache(allow_output_mutation=True)
-def load_faiss_index(path_to_faiss="models/faiss_index.pickle"):
+def load_faiss_index(path_to_faiss="models/stack_faiss_index.pickle"):
     """Load and deserialize the Faiss index."""
     with open(path_to_faiss, "rb") as h:
         data = pickle.load(h)
@@ -28,30 +28,33 @@ def load_faiss_index(path_to_faiss="models/faiss_index.pickle"):
 
 def main():
     # Load data and models
-    data = read_data()
+    df = pd.read_json("data/posts.json")
+    # df['date'] = pd.to_datetime(df['creation_date'])
+    # answers = pd.read_json("data/answers.json")
     model = load_bert_model()
     faiss_index = load_faiss_index()
-
-    st.title("Vector-based searches with Sentence Transformers and Faiss")
+    st.markdown("## StackOverflow search")
+    st.markdown("### Vector-based searches with Sentence Transformers and Faiss")
+    st.write("StackOverflow reference includes " + str(df.shape[0]) + " posts")
 
     # User search
-    user_input = st.text_area("Search box", "covid-19 misinformation and social media")
+    user_input = st.text_area("Search box", "natural language processing")
 
     # Filters
     st.sidebar.markdown("**Filters**")
-    filter_year = st.sidebar.slider("Publication year", 2010, 2021, (2010, 2021), 1)
-    filter_citations = st.sidebar.slider("Citations", 0, 250, 0)
-    num_results = st.sidebar.slider("Number of search results", 10, 50, 10)
+    # filter_year = st.sidebar.slider("Publication year", 2010, 2021, (2010, 2021), 1)
+    filter_answers = st.sidebar.slider("Answers", 0, 25, 0)
+    num_results = st.sidebar.slider("Number of search results", 10, 100, 50)
 
     # Fetch results
     if user_input:
         # Get paper IDs
         D, I = vector_search([user_input], model, faiss_index, num_results)
         # Slice data on year
-        frame = data[
-            (data.year >= filter_year[0])
-            & (data.year <= filter_year[1])
-            & (data.citations >= filter_citations)
+        frame = df[
+            # (data.year >= filter_year[0])
+            # & (data.year <= filter_year[1])
+            (df.answer_count >= filter_answers)
         ]
         # Get individual results
         for id_ in I.flatten().tolist():
@@ -61,13 +64,19 @@ def main():
                 continue
 
             st.write(
-                f"""**{f.iloc[0].original_title}**  
-            **Citations**: {f.iloc[0].citations}  
-            **Publication year**: {f.iloc[0].year}  
-            **Abstract**
-            {f.iloc[0].abstract}
+                f"""**{f.iloc[0].title}**  
+            **Answers**: {f.iloc[0].answer_count}  
+            **Creation UNIX date**: {f.iloc[0].creation_date}  
             """
             )
+            st.markdown(f.iloc[0].body, unsafe_allow_html=True)
+
+            # a = answers[
+            #     (answers.parent_id == f.iloc[0].id)
+            # ]
+            # if f.iloc[0].answer_count > 0:
+            #     st.markdown("## Answers")
+            #     st.markdown(a.body, unsafe_allow_html=True)
 
 
 if __name__ == "__main__":
